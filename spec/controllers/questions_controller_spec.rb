58fe7e78f9b0f6b3372a439e4c31e6bdbeb197c7
bocalo <moldavskiym@yandex.ru>
2021-12-01 16:34:31 +0300

    Make a test to edit action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
@@ -15,8 +17,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
-
     before { get :show, params: { id: question } }
 
     it 'assigns the requested question to @question' do
@@ -29,12 +29,23 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #new' do
     before { get :new }
-    
+
     it 'assigns a new Question to @question' do
       expect(assigns(:question)).to be_a_new(Question)
     end
     it 'renders new view' do
       expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+    it 'renders edit view' do
+      expect(response).to render_template :edit
     end
   end
 end
