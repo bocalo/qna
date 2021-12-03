b5564bd01b342baa6a2ad2f98018622b4a404250
bocalo <moldavskiym@yandex.ru>
2021-12-03 11:30:43 +0300

    Add test to action create in answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+
+    context 'with valid attributes' do
+      it 'saves a new answer in the database' do
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(question.answers, :count).by(1)
+      end
+      it 'redirects to question show view' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'it does not save the answer' do
+        expect { post :create, params: { answer: attributes_for(:answer, :invalid_answer), question_id: question } }.to_not change(question.answers, :count)
+      end
+      it 're-renders new view' do
+        post :create, params: { answer: attributes_for(:answer, :invalid_answer), question_id: question }
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :answer do
     body { "MyText" }
     question { nil }
+
+    trait :invalid_answer do
+      body { nil }
+    end
   end
 end
