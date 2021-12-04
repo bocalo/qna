f769bb3500bca99abb4c41351247aed85b9798ed
bocalo <moldavskiym@yandex.ru>
2021-12-04 17:58:24 +0300

    Delete comments from spec controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,15 +4,6 @@ RSpec.describe AnswersController, type: :controller do
   let(:answer) { create(:answer) }
   let(:question) { create(:question) }
   
-
-  # describe 'GET #new' do
-  #   before { get :new, params: { question_id: question} }
-
-  #   it 'renders new view' do
-  #     expect(response).to render_template :new
-  #   end
-  # end
-
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves a new answer in the database' do
