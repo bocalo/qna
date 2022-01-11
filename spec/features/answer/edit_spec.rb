b6cfa9367ecbedc25447935fd4042fa4c663d538
bocalo <moldavskiym@yandex.ru>
2022-01-11 12:05:15 +0300

    Add tests to attach files for aditing answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -18,37 +18,48 @@ feature 'User can edit his answer', %q{
   end
 
   describe 'Authenticated user' do
-
-    scenario 'edits his answer', js: true do
-      sign_in user
-
-      visit question_path(question)
-
-      click_on 'Edit'
-
-      within '.answers' do
-        fill_in 'Your answer', with: 'edited answer' #, match: :prefer_exact
-        fill_in 'Your answer', with: 'edited answer'
-        click_on 'Save'
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
+    describe 'author' do
+      background do
+        sign_in user
+        visit question_path(question)
       end
-    end
+      scenario 'edits his answer', js: true do
+        click_on 'Edit'
 
-    scenario 'edits his answer with errors', js: true do
-      sign_in user
-      visit question_path(question)
+        within '.answers' do
+          #fill_in 'Your answer', with: 'edited answer' #, match: :prefer_exact
+          fill_in 'Your answer', with: 'edited answer'
+          click_on 'Save'
 
-      click_on 'Edit'
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
 
-      within '.answers' do
-        #fill_in 'Your answer', with: '', match: :prefer_exact
-        fill_in 'Your answer', with: ''
-        #save_and_open_page
-        click_on 'Save'
-        expect(page).to have_content "Body can't be blank"
+      scenario 'edits his answer with errors', js: true do
+        click_on 'Edit'
+
+        within '.answers' do
+          fill_in 'Your answer', with: ''
+          
+          click_on 'Save'
+          expect(page).to have_content "Body can't be blank"
+        end
+      end
+
+      scenario 'edits his answer with files', js: true do
+        click_on 'Edit'
+
+        within '.answers' do
+          fill_in 'Your answer', with: 'edited answer'
+          attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          
+          click_on 'Save'
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
       end
     end
 
