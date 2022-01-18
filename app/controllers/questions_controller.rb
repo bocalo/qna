fd05f8fe4e7be81185b3f03f898e02f909569bab
bocalo <moldavskiym@yandex.ru>
2022-01-18 18:28:36 +0300

    Try to add links into the question file

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.links.new #build
   end
 
   def edit
@@ -47,6 +48,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body, files: [])
+    params.require(:question).permit(:title, :body, files: [], links_attributes: [:name, :url])
   end
 end
