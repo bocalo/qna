46f913154359d1c187c994d0d305ff86a9beaa8e
bocalo <moldavskiym@yandex.ru>
2022-03-02 11:44:06 +0300

    Add test for vote

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,16 +9,16 @@ import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "jquery";
 import "@nathanvda/cocoon";
-const GistClient = require("gist-client");
-const gistClient = new GistClient();
+//const GistClient = require("gist-client");
+//const gistClient = new GistClient();
 import "bootstrap";
-import "./answers";
+import "./answer";
 import "./question";
 import "./votes";
 
 window.jQuery = $;
 window.$ = $;
-window.gistClient = gistClient;
+//window.gistClient = gistClient;
 
 Rails.start();
 Turbolinks.start();
--- spec/features/question/vote_spec.rb
+++ spec/features/question/vote_spec.rb
@@ -17,14 +17,91 @@ feature 'User can vote for a question', %q{
     end
   
     scenario 'votes up for question' do
-      #save_and_open_page
-      within(all('.question')[0]) do
+     # save_and_open_page
+      #sleep 1000
+      within('.question') do
+        click_on 'vote up'
+        #save_and_open_page
+        within '.vote-result' do
+          expect(page).to have_content '1'
+        end
+      end
+    end
+
+    scenario "you can't vote multiple times" do
+      within ".question" do
+        click_on 'vote up'
         click_on 'vote up'
 
         within '.vote-result' do
           expect(page).to have_content '1'
         end
       end
+    end
+
+    scenario "unvote his vote" do
+      within ".question" do
+        click_on 'vote up'
+        click_on 'unvote'
+
+        within '.vote-result' do
+          expect(page).to have_content '0'
+        end
+      end
+    end
+
+    scenario "votes down for question" do
+      within ".question" do
+        click_on 'vote down'
+        
+        within '.vote-result' do
+          expect(page).to have_content '-1'
+        end
+      end
+    end
+
+    scenario "you can't vote multiple times" do
+      within ".question" do
+        click_on 'vote down'
+        click_on 'vote down'
+
+        within '.vote-result' do
+          expect(page).to have_content '-1'
+        end
+      end
+    end
+
+    scenario "you can revotes" do
+      within ".question" do
+        click_on 'vote up'
+        click_on 'unvote'
+        click_on 'vote down'
+
+        within '.vote-result' do
+          expect(page).to have_content '-1'
+        end
+      end
+    end
+  end
+
+  describe 'User is author of question tries to', js: true do
+    scenario "Author hasn't view links" do
+      sign_in(author)
+      visit question_path(question)
+
+      expect(page).to_not have_content 'vote up'
+      expect(page).to_not have_content 'vote down'
+      expect(page).to_not have_content 'unvote'
+    end
+  end
+
+  describe 'Unauthorized user tries to', js: true do
+    scenario "User hasn't view links" do
+      visit question_path(question)
+
+      expect(page).to_not have_content 'vote up'
+      expect(page).to_not have_content 'vote down'
+      expect(page).to_not have_content 'unvote'
     end
   end
 end
