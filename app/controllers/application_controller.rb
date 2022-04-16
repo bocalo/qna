23882b8995d42849d1fe1376ef82390d6038fd3b
bocalo <moldavskiym@yandex.ru>
2022-04-16 17:20:00 +0300

    Add some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  include CanCan
+  
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
-    # format.js { render status: :forbidden }
-    # format.json { render json: exception.message, status: :forbidden }
   end
 
   #check_authorization unless: :devise_controller?
-  skip_authorization_check
+  #skip_authorization_check
 end
+
