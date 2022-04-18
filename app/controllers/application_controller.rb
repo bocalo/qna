a0466d41ba03fc8144e5a532550d15cb172107f9
bocalo <moldavskiym@yandex.ru>
2022-04-18 14:49:31 +0300

    Add method authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
-  include CanCan
   
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
 
-  #check_authorization unless: :devise_controller?
-  #skip_authorization_check
+  check_authorization unless: :devise_controller?
 end
 
